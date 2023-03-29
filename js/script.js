79bbbae0b99ae92d9285cb4ebfc1b67d60b82ebd
Amaan Salim Ansari <amaan0076@gmail.com>
2023-03-29 16:28:47 +0530

    Version 1.1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 let textarea = document.getElementById('text')
 
 textarea.addEventListener('input', function () {
+
+    // To count Letters
     let txt = this.value
     document.getElementById('letter').innerHTML = txt.length
 
-    let word = txt.trim().split(' ')
+    // To count Words
+    let word = txt.trim().split(/\s+/)
     let wordArray = word.filter(function (e) {
         return e != "";
     })
     document.getElementById('word').innerHTML = wordArray.length
 
+
+
+    // To count Spaces
     let space = txt.split(" ")
     let spaceArray = space.filter(function (e) {
-        return e == "";
+        return e == ""
     })
     document.getElementById('space').innerHTML = spaceArray.length
+
+    // To count Special Characters
 })
